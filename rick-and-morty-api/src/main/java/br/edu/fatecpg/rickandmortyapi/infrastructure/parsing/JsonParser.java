1b3c81ef3d43ec97721b7435dc18f027bef0f713
eduardosaraujo1 <eduardosaraujo1@proton.me>
2026-09-06 16:47:38 -0300

    fix: instantiate ObjectMapper singleton instance

--- rick-and-morty-api/src/main/java/br/edu/fatecpg/rickandmortyapi/infrastructure/parsing/JsonParser.java
+++ rick-and-morty-api/src/main/java/br/edu/fatecpg/rickandmortyapi/infrastructure/parsing/JsonParser.java
@@ -4,7 +4,7 @@ import tools.jackson.databind.ObjectMapper;
 
 public class JsonParser {
 
-    private static ObjectMapper mapper;
+    private static ObjectMapper mapper = new ObjectMapper();
 
     public static <T> T parseString(String str, Class<T> clazz) {
         return mapper.readValue(str, clazz);

96eeb75f20f02474078415014434a468c10da704
eduardosaraujo1 <eduardosaraujo1@proton.me>
2026-09-06 11:58:30 -0300

    task: logger utility

--- rick-and-morty-api/src/main/java/br/edu/fatecpg/rickandmortyapi/RickAndMortyApiApplication.java
+++ rick-and-morty-api/src/main/java/br/edu/fatecpg/rickandmortyapi/RickAndMortyApiApplication.java
@@ -13,6 +13,6 @@ public class RickAndMortyApiApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Hello, world!");
+        //
     }
 }
--- rick-and-morty-api/src/main/java/br/edu/fatecpg/rickandmortyapi/infrastructure/logging/Logger.java
+++ rick-and-morty-api/src/main/java/br/edu/fatecpg/rickandmortyapi/infrastructure/logging/Logger.java
@@ -1,5 +1,48 @@
 package br.edu.fatecpg.rickandmortyapi.infrastructure.logging;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 public class Logger {
-    //
+
+    private static Logger _instance;
+
+    private Logger() {}
+
+    public static Logger instance() {
+        if (_instance == null) {
+            _instance = new Logger();
+        }
+
+        return _instance;
+    }
+
+    private ZonedDateTime getTimestamp() {
+        return ZonedDateTime.of(LocalDateTime.now(), ZoneId.systemDefault());
+    }
+
+    public void log(String message) {
+        log(message, "registros.log");
+    }
+
+    public void log(String message, String fileName) {
+        // Estrutura: [Timestamp] Mensagem
+        ZonedDateTime timestamp = getTimestamp();
+
+        try (
+            FileWriter writer = new FileWriter(
+                fileName,
+                StandardCharsets.UTF_8,
+                true
+            )
+        ) {
+            writer.write("[%s] %s\n".formatted(timestamp.toString(), message));
+        } catch (IOException e) {
+            System.out.println("Não foi possível escrever um registro.");
+        }
+    }
 }

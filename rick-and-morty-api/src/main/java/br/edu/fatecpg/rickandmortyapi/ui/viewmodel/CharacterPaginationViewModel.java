708038889b62a71b0a1c2f84e2f466ae8b562ae8
eduardosaraujo1 <eduardosaraujo1@proton.me>
2026-09-06 20:59:43 -0300

    task: implement pagination view model

--- rick-and-morty-api/src/main/java/br/edu/fatecpg/rickandmortyapi/ui/viewmodel/CharacterPaginationViewModel.java
+++ rick-and-morty-api/src/main/java/br/edu/fatecpg/rickandmortyapi/ui/viewmodel/CharacterPaginationViewModel.java
@@ -1,6 +1,10 @@
 package br.edu.fatecpg.rickandmortyapi.ui.viewmodel;
 
 import br.edu.fatecpg.rickandmortyapi.data.CharacterRepository;
+import br.edu.fatecpg.rickandmortyapi.domain.exceptions.CharacterListUnreachableException;
+import br.edu.fatecpg.rickandmortyapi.domain.exceptions.InvalidCharacterListResponseException;
+import br.edu.fatecpg.rickandmortyapi.domain.model.SeriesCharacter;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CharacterPaginationViewModel {
@@ -11,9 +15,60 @@ public class CharacterPaginationViewModel {
 
     private int _currentPage = 1;
     private int _cachedPage = 1;
-    private List<Character> cache;
+    private List<SeriesCharacter> _characterList = null;
+
+    private boolean _hasFetchError = false;
+
+    // Will include a "cachedItemCount" in the future to define how many pages there are.
 
     public CharacterPaginationViewModel() {
         _repo = new CharacterRepository();
+    }
+
+    public List<SeriesCharacter> getCharacterList() {
+        _hasFetchError = false;
+
+        try {
+            if (_characterList == null || _cachedPage != _currentPage) {
+                _characterList = _repo.listCharacters(
+                    ROWS_PER_PAGE,
+                    ROWS_PER_PAGE * (_currentPage - 1)
+                );
+                _cachedPage = _currentPage;
+            }
+        } catch (
+            CharacterListUnreachableException
+            | InvalidCharacterListResponseException e
+        ) {
+            // It is possible we have just reached the end of the list and no error occurred
+            // (I did not realize I'd need  registryCount to validate if the next fetch operation.
+            // I'm not changing the repository yet I've spent enough time on that)
+            // For now let's presume it actually went wrong, and show an error message
+
+            _characterList = new ArrayList<SeriesCharacter>();
+            _cachedPage = -1;
+            _hasFetchError = true;
+        }
+
+        return _characterList;
+    }
+
+    public boolean hasFetchError() {
+        return _hasFetchError;
+    }
+
+    public boolean nextPage() {
+        // In the future: compare with Math.ceil(cachedItemCount / ROWS_PER_PAGE)
+        ++_currentPage;
+        return true;
+    }
+
+    public boolean previousPage() {
+        if (_currentPage < 1) {
+            return false;
+        }
+
+        --_currentPage;
+        return true;
     }
 }
